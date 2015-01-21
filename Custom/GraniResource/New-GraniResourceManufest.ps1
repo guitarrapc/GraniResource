8045711b836a722b4bb1fb1b221279df664f7b55
guitarrapc <cherishacquire@outlook.com>
2015-01-22 06:36:47 +0900

    Update GraniResource to ver.2.5

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,15 +1,15 @@
 ﻿$moduleName = 'GraniResource'
 $script:moduleManufest = @{
-    Path                   = ".\$ModuleName.psd1"
-    ModuleVersion          = '2.0'
-    Author                 = 'guitarrapc'
-    CompanyName            = 'Grani'
-    Description            = 'Grani Resource for Windows Environment Setup'
-    PowerShellVersion      = '4.0'
-    CLRVersion             = '4.0'
-    RequiredModules = @()
-    FunctionsToExport      = @('*')
-    CmdletsToExport = '*'
+    Path              = ".\$ModuleName.psd1"
+    ModuleVersion     = '2.5'
+    Author            = 'guitarrapc'
+    CompanyName       = 'Grani'
+    Description       = 'DSC Resource for Windows Configuration Management.'
+    PowerShellVersion = '4.0'
+    CLRVersion        = '4.0'
+    RequiredModules   = @()
+    FunctionsToExport = @('*')
+    CmdletsToExport   = '*'
 }
 
 New-ModuleManifest @moduleManufest
