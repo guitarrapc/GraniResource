1269a59118d6e817a72045bbdeed41f1faf8d032
guitarrapc <cherishacquire@outlook.com>
2015-01-22 05:58:55 +0900

    Clean up and divide into Separate Project for GraniResource, GraniResource.Test, GraniResource.Designer.
    Added cDownload Configuration Sample

---
+++ Custom/GraniResource.Test/Grani_Download/Configuration/ConfigurationSample.ps1
@@ -0,0 +1,12 @@
+﻿configuration DownloadGitHubRawContent
+{
+    Import-DscResource -ModuleName GraniResource
+    cDownload hoge
+    {
+        DestinationPath = "C:\Tools\README.md"
+        Uri = "https://raw.githubusercontent.com/guitarrapc/DSCResources/master/README.md"
+    }
+}
+
+DownloadGitHubRawContent
+Start-DscConfiguration -Wait -Verbose -Force -Path DownloadGitHubRawContent
