30dd330b88ea007f4e5f7b85dfbd68b282dbdb12
guitarrapc <cherishacquire@outlook.com>
2015-01-24 00:31:46 +0900

    Update to ver.2.6.0. Added Grani_GitHubApiContent Resource.

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,7 +1,7 @@
 ﻿$moduleName = 'GraniResource'
 $script:moduleManufest = @{
     Path              = ".\$ModuleName.psd1"
-    ModuleVersion     = '2.5'
+    ModuleVersion     = '2.6'
     Author            = 'guitarrapc'
     CompanyName       = 'Grani'
     Description       = 'DSC Resource for Windows Configuration Management.'
