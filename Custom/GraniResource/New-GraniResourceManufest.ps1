4e0f79878b9eeaa8869b6667b57890b029118d98
guitarrapc <cherishacquire@outlook.com>
2015-01-29 23:07:35 +0900

    Update GraniResource to 2.7.1

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,7 +1,7 @@
 ﻿$moduleName = 'GraniResource'
 $script:moduleManufest = @{
     Path              = ".\$ModuleName.psd1"
-    ModuleVersion     = '2.7'
+    ModuleVersion     = '2.7.1'
     Author            = 'guitarrapc'
     CompanyName       = 'Grani'
     Description       = 'DSC Resource for Windows Configuration Management.'
