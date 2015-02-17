935b91410c1845d9731e34dd7ac378451433ec0d
guitarrapc <cherishacquire@outlook.com>
2015-02-17 18:19:37 +0900

    update README.md

--- Custom/GraniResource.Test/Grani_S3Content/Configuration/withAction/ConfigurationSample.ps1
+++ Custom/GraniResource.Test/Grani_S3Content/Configuration/withAction/ConfigurationSample.ps1
@@ -6,8 +6,8 @@
         S3BucketName = "YourBucketName"
         Key = "ObjectName"
         DestinationPath = "c:\Path\To\Save\Content.log"
-        PreAction = '"PreAction : {0}" -f (Get-Date)'
-        PostAction = '"PostAction : {0}" -f (Get-Date)'
+        PreAction = {"PreAction : {0}" -f (Get-Date)}
+        PostAction = {"PostAction : {0}" -f (Get-Date)}
     }
 }
 
