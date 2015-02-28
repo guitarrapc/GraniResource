6a152631cbeace8bc801a1c8df0d827324303b3c
guitarrapc <cherishacquire@outlook.com>
2015-03-01 00:34:53 +0900

    Sync Directory Strucure with psm1

---
+++ Custom/GraniResource.Test/DSCResources/Grani_ScheduleTask/ValidateTaskPathLastChar.Tests.ps1
@@ -0,0 +1,17 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Grani_ScheduleTask : ValidateTaskPathLastChar" {
+
+    Context "GetPathItemType" {
+
+        It "none \ last input value will output as last character with \" {
+            ValidateTaskPathLastChar -taskPath "hoge" | Should be "hoge\"
+        }
+
+        It "with \ last input value will never change" {
+            ValidateTaskPathLastChar -taskPath "hoge\" | Should be "hoge\"
+        }
+    }
+}
--- Custom/GraniResource.Test/Grani_ScheduleTask/ValidateTaskPathLastChar.ps1
+++
@@ -1,6 +0,0 @@
-﻿function ValidateTaskPathLastChar ($taskPath)
-{
-    $lastChar = [System.Linq.Enumerable]::ToArray($taskPath) | select -Last 1
-    if ($lastChar -ne "\"){ return $taskPath + "\" }
-    return $taskPath
-}
