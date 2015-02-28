502760bfef59d5acb291cf0c024aae272cb9b034
guitarrapc <cherishacquire@outlook.com>
2015-03-01 07:17:33 +0900

    Update GraniResource to ver.2.9.1
    Support All property change detection for Grani_ScheduledTask Resource.

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,7 +1,7 @@
 ﻿$moduleName = 'GraniResource'
 $script:moduleManufest = @{
     Path              = ".\$ModuleName.psd1"
-    ModuleVersion     = '2.9.0'
+    ModuleVersion     = '2.9.1'
     Author            = 'guitarrapc'
     CompanyName       = 'Grani'
     Description       = 'DSC Resource for Windows Configuration Management.'
