e1cca7cb60e30dfcd6ca5401d35ef9835baea08f
guitarrapc <cherishacquire@outlook.com>
2015-03-02 23:56:52 +0900

    Added Pester test

---
+++ Custom/GraniResource.Test/DSCResources/Grani_PendingReboot/Configuration/ConfigurationSample.ps1
@@ -0,0 +1,13 @@
+﻿configuration PendingReboot
+{
+    Import-DscResource -ModuleName GraniResource
+    cPendingReboot PendingReboot
+    {
+        Name = "Pending Reboot"
+        WaitTimeSec = 5
+        Force = $true
+        WhatIf = $false
+    }
+}
+PendingReboot
+Start-DscConfiguration -Path PendingReboot -Wait -Force -Verbose
