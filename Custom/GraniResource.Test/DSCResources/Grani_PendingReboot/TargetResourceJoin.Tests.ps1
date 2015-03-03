adf337b6b3e365bb2f60e8fa6c26c45d99d8caa0
guitarrapc <cherishacquire@outlook.com>
2015-03-03 13:08:06 +0900

    Added Test

--- Custom/GraniResource.Test/DSCResources/Grani_PendingReboot/TargetResourceJoin.Tests.ps1
+++ Custom/GraniResource.Test/DSCResources/Grani_PendingReboot/TargetResourceJoin.Tests.ps1
@@ -8,6 +8,7 @@ Describe "Grani_PendingReboot : *-TargetResource" {
     $waitTimeSec = 1
     $force = $true
     $whatIf = $true
+    $TriggerPendingFileRename = $false
 
     $componentBasedServicing = @{
         Path = 'registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\'
@@ -35,7 +36,7 @@ Describe "Grani_PendingReboot : *-TargetResource" {
         }
 
         It "Get-TargetResource should return Ensure : Present" {
-            (Get-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf).Ensure | should be ([Ensuretype]::Present.ToString())
+            (Get-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf).Ensure | should be "Absent"
         }
 
         It "Test-TargetResource should return true" {
@@ -44,8 +45,8 @@ Describe "Grani_PendingReboot : *-TargetResource" {
 
         Set-ItemProperty @pendingFileRenameOperations
         $result = Get-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf
-        It "Get-TargetResource should return Ensure : $true" {
-            $result.Ensure | should be $true
+        It "Get-TargetResource should return Ensure : Absent" {
+            $result.Ensure | should be "Absent"
         }
 
         It "Get-TargetResource should return Name : $name1" {
@@ -58,6 +59,26 @@ Describe "Grani_PendingReboot : *-TargetResource" {
 
         It "Get-TargetResource should return RebootNodeIfNeeded : $rebootNodeIfNeeded" {
             $result.RebootNodeIfNeeded | should be $rebootNodeIfNeeded
+        }
+
+        It "Get-TargetResource should return TriggerComponentBasedServicing :  $true" {
+            $result.TriggerComponentBasedServicing | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerWindowsUpdate :  $true" {
+            $result.TriggerWindowsUpdate | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerPendingFileRename :  $true" {
+            $result.TriggerPendingFileRename | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerPendingComputerRename :  $true" {
+            $result.TriggerPendingComputerRename | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerCcmClientSDK :  $true" {
+            $result.TriggerCcmClientSDK | should be $true
         }
 
         It "Get-TargetResource should return ComponentBasedServicing : $false" {
@@ -76,7 +97,7 @@ Describe "Grani_PendingReboot : *-TargetResource" {
             $result.PendingComputerRename | should be $false
         }
 
-        It "Test-TargetResource should return false" {
+        It "Test-TargetResource should return $false" {
             Test-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf | should be $false
         }
 
@@ -85,6 +106,70 @@ Describe "Grani_PendingReboot : *-TargetResource" {
                 Set-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf
                 Remove-ItemProperty $pendingFileRenameOperations.Path -Name $pendingFileRenameOperations.Name
             } | should not Throw
+        }
+    }
+
+    Context "skip pendingFileRenameOperations environment." {
+        Set-ItemProperty @pendingFileRenameOperations
+        $result = Get-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf -TriggerPendingFileRename $TriggerPendingFileRename 
+        It "Get-TargetResource should return Ensure : $true" {
+            $result.Ensure | should be $true
+        }
+
+        It "Get-TargetResource should return Name : $name1" {
+            $result.Name | should be $name1
+        }
+
+        It "Get-TargetResource should return WaitTimeSec : $waitTimeSec" {
+            $result.WaitTimeSec | should be $waitTimeSec
+        }
+
+        It "Get-TargetResource should return RebootNodeIfNeeded : $rebootNodeIfNeeded" {
+            $result.RebootNodeIfNeeded | should be $rebootNodeIfNeeded
+        }
+
+        It "Get-TargetResource should return TriggerComponentBasedServicing :  $true" {
+            $result.TriggerComponentBasedServicing | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerWindowsUpdate :  $true" {
+            $result.TriggerWindowsUpdate | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerPendingFileRename :  $true" {
+            $result.TriggerPendingFileRename | should be $false
+        }
+
+        It "Get-TargetResource should return TriggerPendingComputerRename :  $true" {
+            $result.TriggerPendingComputerRename | should be $true
+        }
+
+        It "Get-TargetResource should return TriggerCcmClientSDK :  $true" {
+            $result.TriggerCcmClientSDK | should be $true
+        }
+
+        It "Get-TargetResource should return ComponentBasedServicing : $false" {
+            $result.ComponentBasedServicing | should be $false
+        }
+
+        It "Get-TargetResource should return ComponentBasedServicing : $false" {
+            $result.ComponentBasedServicing | should be $false
+        }
+
+        It "Get-TargetResource should return WindowsUpdate : $false" {
+            $result.WindowsUpdate | should be $false
+        }
+
+        It "Get-TargetResource should return PendingFileRename : $false" {
+            $result.PendingFileRename | should be $false
+        }
+
+        It "Get-TargetResource should return PendingComputerRename : $false" {
+            $result.PendingComputerRename | should be $false
+        }
+
+        It "Test-TargetResource should return $true" {
+            Test-TargetResource -Name $name1 -WaitTimeSec $waitTimeSec -Force $force -WhatIf $whatIf -TriggerPendingFileRename $TriggerPendingFileRename | should be $true
         }
     }
 }
