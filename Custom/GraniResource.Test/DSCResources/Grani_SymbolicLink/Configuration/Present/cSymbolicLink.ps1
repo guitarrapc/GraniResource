f582cde4c14ea9510950db1425a996cf2fdc6ada
guitarrapc <cherishacquire@outlook.com>
2015-03-09 17:28:22 +0900

    Added Grani_SymbolicLink Resource, Pester test complete.

---
+++ Custom/GraniResource.Test/DSCResources/Grani_SymbolicLink/Configuration/Present/cSymbolicLink.ps1
@@ -0,0 +1,13 @@
+﻿configuration present
+{
+    Import-DscResource -ModuleName GraniResource
+    cSymbolicLink hoge
+    {
+        SourcePath = "C:\Logs\DSC"
+        DestinationPath = "C:\DSC"
+        Ensure = "Present"
+    }
+}
+
+present
+Start-DscConfiguration -Path present -Wait -Verbose -Force
