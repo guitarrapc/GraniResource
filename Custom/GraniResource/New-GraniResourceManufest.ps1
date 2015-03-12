649607c4054e33e1107791de67af924b5349c1bc
guitarrapc <cherishacquire@outlook.com>
2015-03-12 21:29:24 +0900

    update to ver.3.0.1

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,7 +1,7 @@
 ﻿$moduleName = 'GraniResource'
 $script:moduleManufest = @{
     Path              = ".\$ModuleName.psd1"
-    ModuleVersion     = '3.0.0'
+    ModuleVersion     = '3.0.1'
     Author            = 'guitarrapc'
     CompanyName       = 'Grani'
     Description       = 'DSC Resource for Windows Configuration Management.'
