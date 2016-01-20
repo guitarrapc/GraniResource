5a93b75591a8b8bb00f438190006feebfdcef0d7
guitarrapc <cherishacquire@outlook.com>
2016-01-21 03:29:00 +0900

    Update psd1 for PowerShellGet Publishing.

--- Custom/GraniResource/New-GraniResourceManufest.ps1
+++ Custom/GraniResource/New-GraniResourceManufest.ps1
@@ -1,15 +1,39 @@
 ﻿$moduleName = 'GraniResource'
+$moduleVersion = '3.7.3'
 $script:moduleManufest = @{
-    Path              = ".\$ModuleName.psd1"
-    ModuleVersion     = '3.7.3'
-    Author            = 'guitarrapc'
-    CompanyName       = 'Grani'
-    Description       = 'DSC Resource for Windows Configuration Management.'
-    PowerShellVersion = '4.0'
-    CLRVersion        = '4.0'
-    RequiredModules   = @()
-    FunctionsToExport = @('*')
-    CmdletsToExport   = '*'
+    Path                 = ".\$ModuleName.psd1"
+    ModuleVersion        = $moduleVersion
+    Author               = 'guitarrapc'
+    CompanyName          = 'Grani'
+    Description          = 'DSC Resource for Windows Configuration Management.'
+    PowerShellVersion    = '4.0'
+    CLRVersion           = '4.0'
+    RequiredModules      = @()
+    FunctionsToExport    = @('*')
+    CmdletsToExport      = '*'
+    ReleaseNotes         = "https://github.com/guitarrapc/DSCResources/releases/tag/ver.$moduleVersion"
+    ProjectUri           = "https://github.com/guitarrapc/DSCResources"
+    LicenseUri           = "https://github.com/guitarrapc/DSCResources/blob/master/LICENSE"
+    DscResourcesToExport = @(
+        "cACL",
+        "cDomainJoin",
+        "cDotNetFramework",
+        "cDownload",
+        "cGitHubApiContent",
+        "cHostsFile",
+        "cInheritACL",
+        "cPendingReboot",
+        "cPfxImport",
+        "cRegistryKey",
+        "cS3Content",
+        "cScheduleTask",
+        "cScheduleTaskLog",
+        "cSymbolicLink",
+        "cTCPAckFrequency",
+        "cTopShelf",
+        "cWebPI",
+        "cWebPILauncher"
+    )
 }
 
 New-ModuleManifest @moduleManufest
--- Custom/README.md
+++ Custom/README.md
@@ -3,14 +3,22 @@ GraniResource
 
 This directory includes Custom DSC Resource created by guitarrapc and used in Grani Production.
 
+Installation
+----
+
+You can retrieve Resource through [PoweShellGet](https://www.powershellgallery.com/packages/GraniResource).
+```powershell
+Install-Module GraniResource
+```
+
 Directory Tree
 ----
-M
+
 DirectoryName | Description
 ----|----
 Designer | Contains xDSCResourceDesigner script to create ```*.schame.mof``` and ```*.psm1```.
 GraniResource | Contains  DSC Resource source code.
-Package | Contains Zip file for relase every tags.
+Package | Contains Zip file for release tags.
 Test | Contains Pester and Configuration Tests for each DSC Resource.
 
 Where can I see how to usage of DSCResource?
