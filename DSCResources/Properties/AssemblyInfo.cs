8c6a2b44dcbc30ce2f9d72bcf54fe343c372bcf9
guitarrapc <cherishacquire@outlook.com>
2018-02-14 15:55:38 +0900

    Release(3.9.0) New release adding cS3Content Property

--- DSCResources/Properties/AssemblyInfo.cs
+++ DSCResources/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DSCResource")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("guitarrapc")]
 [assembly: AssemblyProduct("DSCResource")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.8.4")]
+[assembly: AssemblyVersion("3.7.9.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
