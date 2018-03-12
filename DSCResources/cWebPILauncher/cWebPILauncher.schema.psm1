191d114209bb109a1e183c7fabfeec658a9712b7
guitarrapc <cherishacquire@outlook.com>
2018-03-12 17:08:37 +0900

    convert csproj to new stytle

--- DSCResources/cWebPILauncher/cWebPILauncher.schema.psm1
+++ DSCResources/cWebPILauncher/cWebPILauncher.schema.psm1
@@ -3,13 +3,13 @@
     param
     (
         [Parameter(Mandatory = $false, helpMessage = "Install and search from Reg by Get-DSCModuleProductId")]
-        [string]$ProductId = "4D84C195-86F0-4B34-8FDE-4A17EB41306A", 
+        [string]$ProductId = "4D84C195-86F0-4B34-8FDE-4A17EB41306A",
 
         [Parameter(Mandatory = $false)]
         [string]$InstallerUri = "http://go.microsoft.com/fwlink/?LinkId=255386"
     )
 
-    function Get-RedirectedUrl ([String]$URL) { 
+    function Get-RedirectedUrl ([String]$URL) {
         $request = [System.Net.WebRequest]::Create($InstallerUri)
         $request.AllowAutoRedirect = $false
         $response = $request.GetResponse()
